8cbad6be818f271a2110a45d4222d976f995957b
Cash Coffman <116964726+Moneydollar@users.noreply.github.com>
2023-07-23 22:19:31 -0400

    Added new button to the GUI that opens up the directory that downloaded videos are stored in for easy viewing, and I imporved the download status label. It now displays when a download is ready and when the program can download another file.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import tkinter as tk
 import yt_dlp
 import os
 import ffmpeg
+import subprocess
 import threading  # For multithreading
 
 
@@ -9,6 +10,7 @@ class videoDownloader:
     def __init__(self, height, width, title):
         self.root = tk.Tk()
         self.isDownloading = False
+        self.loop_times = 0
         self.height = height
         self.icon = "assets/icon.ico"
         tk.Tk.iconbitmap(self.root, default=self.icon)
@@ -25,30 +27,48 @@ class videoDownloader:
             self.root, text="Enter the video link to the audio you want to download"
         )
         self.label.pack(pady=10)
+        self.open_button = tk.Button(
+            text="View Downloaded Audio", command=self.open_directory
+        )
+        self.open_button.pack(pady=10)
         self.entry_box.bind("<Return>", self.on_enter_pressed)
 
         # Create a Exit Button
 
         self.exit_button = tk.Button(self.root, text="Exit", command=self.on_closing)
         self.exit_button.pack(pady=10)
+        self.download_label = tk.Label(self.root, text="Ready to Download")
+        self.download_label.pack(pady=10)
 
     def get_song(self):
-        try:
-            song = self.entry_box.get()
-            return song
-        except Exception:
-            song = "Invalid Link, Please Try Again"
         song = self.entry_box.get()
         return song
+
+    def open_directory(self):
+        home = os.path.expanduser("~")
+        new_directory = os.path.join(home, "ffmpeg_downloads")
+        subprocess.run(f"explorer {new_directory}")
+        pass
 
     def on_enter_pressed(self, event):
         self.isDownloading = True
         song = self.entry_box.get()
+        if self.isDownloading and self.loop_times == 0:
+            try:
+                self.download_label.config(text="Downloading... Please Wait")
+                threading.Thread(target=self.get_audio).start()
+            except yt_dlp.utils.DownloadError:
+                self.download_label.config(text="Download Failed, Link is Invalid")
+            self.loop_times += 1
+        else:
+            return False
+
+    def on_closing(self):
         if self.isDownloading:
-            self.download_label = tk.Label(self.root)
-            self.download_label.config(text="Downloading... Please Wait")
-            self.download_label.pack(pady=10)
-            threading.Thread(target=self.get_audio).start()
+            # If downloading is in progress, cancel closing and wait for the download to complete
+            print("Cannot quit. Download in progress.")
+        else:
+            self.root.destroy()
 
     # def refresh(self):
     #     self.root.update()
@@ -83,9 +103,12 @@ class videoDownloader:
         if os.path.isfile(video_file_path):
             self.download_label.config(text="Download Complete")
         self.entry_box.delete(0, tk.END)
-        self.download_label.pack_forget()
+        self.download_label.config(
+            text="Download Complete, Ready to Process another Video"
+        )  # Update the label
         os.remove(video_file_path)
         self.isDownloading = False
+        self.loop_times = 0
         return processed_Audio
 
     def run(self):
@@ -96,14 +119,7 @@ class videoDownloader:
         except tk.TclError:
             pass
 
-    def on_closing(self):
-        if self.isDownloading:
-            # If downloading is in progress, cancel closing and wait for the download to complete
-            print("Cannot quit. Download in progress.")
-        else:
-            self.root.destroy()
-
 
 # root.mainloop()
-app = videoDownloader(400, 170, "YouTube Downloader")
+app = videoDownloader(400, 220, "YouTube Downloader")
 app.run()
